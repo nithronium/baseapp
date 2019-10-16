2c03d47cb8a02643360dae38e2fc5049e7388614
Mikhail Zyranau <mikhail.zyranau@dogowrld.com>
2019-10-16 17:53:07 +0300

    Feature: Add copyrights into referral page into footer

---
+++ src/custom/components/Referral/Footer/Copyright/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+const Copyright = () => {
+    return (
+        <div className="pg-referral-screen__footer-copyright">
+            <div>
+                Emirex.com, BME Technologies OU is a licensed crypto to fiat exchange, authorised by the Central Bank of Estonia.
+            </div>
+            <div>© 2019 Emirex.com All rights reserved.</div>
+        </div>
+    );
+};
+
+export { Copyright };
---
+++ src/custom/components/Referral/Footer/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { Copyright } from './Copyright';
+// import HeaderComponent from './Header';
+// import FooterBodyComponent from './FooterBody';
+
+const Footer = () => {
+    return (
+        <div className="pg-referral-screen__footer">
+            {/* <MobileStartTrading /> */}
+            {/* <HeaderComponent /> */}
+            {/* <FooterBodyComponent />  */}
+            <Copyright />
+        </div>
+    );
+};
+
+export { Footer };
