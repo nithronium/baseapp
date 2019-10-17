15c1c84bf7ea0e24ddb9d9a67cb441a38574cfdf
Mikhail Zyranau <mikhail.zyranau@dogowrld.com>
2019-10-17 13:30:59 +0300

    Feature: Add footer from site to baseapp referral page

--- src/custom/components/Referral/Footer/index.tsx
+++ src/custom/components/Referral/Footer/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import facebook = require('../../../assets/images/referral/icons/facebook.svg');
 import instagram = require('../../../assets/images/referral/icons/instagram.svg');
 import linkedin = require('../../../assets/images/referral/icons/linkedin.svg');
@@ -7,6 +8,7 @@ import reddit = require('../../../assets/images/referral/icons/reddit.svg');
 import telegram = require('../../../assets/images/referral/icons/Telegram.svg');
 import twitter = require('../../../assets/images/referral/icons/twitter.svg');
 import logo = require('../../../assets/images/referral/logo-emirex.svg');
+
 const Header = () => {
     return (
         <div className="logo">
@@ -14,6 +16,7 @@ const Header = () => {
         </div>
     );
 };
+
 /******** start component */
 const Copyright = () => {
     return (
@@ -39,6 +42,7 @@ const Licenses = () => {
         </div>
     );
 };
+
 //tslint:disable
 /******** start component */
 const links6 = [
@@ -101,14 +105,118 @@ const GetInTouch = () => {
 
 /******** start component */
 
+const Address = () => {
+    return (
+        <div className="address">
+            <h5>Address</h5>
+            <p>47, Peterburi tee, Lasnamäe linnaosa, Harju maakond, Tallinn, Estonia</p>
+        </div>
+    );
+};
+
+const title1 = 'Company';
+const links1 = [
+    { key: '', href: '/en/about', label: 'About' },
+    { key: '', href: '/en/whitepaper', label: 'White Paper' },
+    /* { href: '/team', label: 'footer.links.team'},
+  { href: '/careers', label: 'footer.links.careers'},
+  { href: '/blog', label: 'footer.links.blog'},*/
+    { key: '', href: '/referral', label: 'Referrlal Giveaway', className: 'gold' },
+].map((link, index) => {
+    link.key = `footer-link-${index}-${link.href}-${link.label}`;
+    return link;
+});
+
+const title2 = 'Our Services';
+const links2 = [
+    { key: '', href: '/trading', label: 'Trading Platform' },
+    { key: '', href: 'https://emrx.emirex.com', label: 'EMRX Token' },
+    { key: '', href: 'https://advisory.emirex.com', label: 'Advisory' },
+    /*  { href: '/listing', label: 'footer.links.listing'},*/
+].map((link, index) => {
+    link.key = `footer-link-${index}-${link.href}-${link.label}`;
+    return link;
+});
+
+const title3 = 'Buy/Sell Crypto';
+const links3 = [
+    { key: '', href: '/en/trading/btcusdt', label: 'Buy Bitcoin' },
+    { key: '', href: '/en/trading/ethusdt', label: 'Buy Ethereum' },
+    /*{ href: '/trading', label: 'footer.links.buyRipple'},
+  { href: '/trading', label: 'footer.links.buyLitecoin'},
+  { href: '/trading', label: 'footer.links.buyEmrx'},*/
+].map((link, index) => {
+    link.key = `footer-link-${index}-${link.href}-${link.label}`;
+    return link;
+});
+
+const title4 = 'Support';
+const links4 = [
+    { key: '', href: '/en/faq', label: 'FAQ' },
+    { key: '', href: 'mailto:support@emirex.com', label: 'Submit a request' },
+    { key: '', href: '/en/fees', label: 'Fees' },
+].map((link, index) => {
+    link.key = `footer-link-${index}-${link.href}-${link.label}`;
+    return link;
+});
+
+const title5 = 'Legal';
+const links5 = [
+    { key: '', href: '/en/terms', label: 'Terms of Use' },
+    { key: '', href: '/en/privacy', label: 'Privacy Policy' },
+    { key: '', href: '/en/kyc_policy', label: 'KYC/AML policy' },
+].map((link, index) => {
+    link.key = `footer-link-${index}-${link.href}-${link.label}`;
+    return link;
+});
+
+const LinksList = ({ links }) => {
+    return (
+        <ul className="footer-link-list">
+            {links.map(({ key, href, label, className }) => (
+                <li key={key}>
+                    <a className={className} href={href}>
+                        {label}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+const LinksColumn = ({ title, links }) => {
+    return (
+        <div className="link-list-column">
+            <h5>{title}</h5>
+            <LinksList links={links} />
+        </div>
+    );
+};
+
+const FooterBody = () => {
+    return (
+        <div className="footer-row">
+            <LinksColumn title={title1} links={links1} />
+            <LinksColumn title={title2} links={links2} />
+            <LinksColumn title={title3} links={links3} />
+            <div>
+                <LinksColumn title={title4} links={links4} />
+                <LinksColumn title={title5} links={links5} />
+            </div>
+            <div>
+                <GetInTouch />
+                <Address />
+            </div>
+        </div>
+    );
+};
+
 const Footer = () => {
     return (
         <div id="footer">
             {/* <MobileStartTrading /> */}
             <Header />
-            {/* <FooterBodyComponent />  */}
-
-            <GetInTouch />
+            <FooterBody />
             <Licenses />
             <Copyright />
         </div>
