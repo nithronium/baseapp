23d114095e4d4f2689946b26d9afd0d13b363515
Mikhail Zyranau <mikhail.zyranau@dogowrld.com>
2019-11-04 11:41:54 +0300

    Feature: add conditions to signUpSaga

--- src/modules/user/auth/sagas/signUpSaga.ts
+++ src/modules/user/auth/sagas/signUpSaga.ts
@@ -8,12 +8,27 @@ const signUpConfig: RequestOptions = {
     apiVersion: 'barong',
 };
 
+const baseUrl = window.document.location.origin;
+//tslint:disable
 export function* signUpSaga(action: SignUpFetch) {
+    if (action.payload.refid) {
+    }
     try {
-        yield call(API.post(signUpConfig), '/identity/users', action.payload);
-        yield put(signUpRequireVerification({ requireVerification: true }));
+        if (!action.payload.refid) {
+            yield call(API.post(signUpConfig), '/identity/users', action.payload);
+            yield put(signUpRequireVerification({ requireVerification: true }));
+        } else {
+            const existCode = yield fetch(`${baseUrl}/api/v2/referral-code?user_uid=IDBF19BD26D5&referral_code=IDBF19BD26D5`);
+            console.log(existCode);
+            if (existCode.ok) {
+                yield call(API.post(signUpConfig), '/identity/users', action.payload);
+                yield put(signUpRequireVerification({ requireVerification: true }));
+            } else {
+                yield put(alertPush({ message: ['Referral Code Invalid'], type: 'error' }));
+            }
+        }
     } catch (error) {
         yield put(signUpError(error));
-        yield put(alertPush({message: error.message, code: error.code, type: 'error'}));
+        yield put(alertPush({ message: error.message, code: error.code, type: 'error' }));
     }
 }
