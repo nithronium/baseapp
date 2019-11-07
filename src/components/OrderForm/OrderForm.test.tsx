be042f8f3e13b0260f1661c8de7f6c161f00fcd8
Mikhail Zyranau <mikhail.zyranau@digiwrld.com>
2019-11-07 13:17:17 +0300

    Fix: hotfix make tests without fees

--- src/components/OrderForm/OrderForm.test.tsx
+++ src/components/OrderForm/OrderForm.test.tsx
@@ -8,10 +8,7 @@ import { OrderForm, OrderFormProps } from './';
 type DropdownElem = number | string | React.ReactNode;
 type FormType = 'buy' | 'sell';
 
-const defaultOrderTypes: DropdownElem[] = [
-    'Limit',
-    'Market',
-];
+const defaultOrderTypes: DropdownElem[] = ['Limit', 'Market'];
 
 const defaultProps = {
     orderTypes: defaultOrderTypes,
@@ -27,12 +24,11 @@ const defaultProps = {
     from: 'btc',
     to: 'eth',
     onSubmit: spy(),
-    proposals: [['10','1']],
+    proposals: [['10', '1']],
     listenInputPrice: spy(),
 };
 
-const setup = (props: Partial<OrderFormProps> = {}) =>
-    shallow(<OrderForm {...{ ...defaultProps, ...props }} />);
+const setup = (props: Partial<OrderFormProps> = {}) => shallow(<OrderForm {...{ ...defaultProps, ...props }} />);
 
 describe('OrderForm', () => {
     it('should match snapshot', () => {
@@ -60,10 +56,30 @@ describe('OrderForm', () => {
     it('should render percentage buttons', () => {
         const wrapper = setup();
         const percentageButton = wrapper.find(PercentageButton);
-        expect(percentageButton.at(0).props().label.toLowerCase()).toBe('25%');
-        expect(percentageButton.at(1).props().label.toLowerCase()).toBe('50%');
-        expect(percentageButton.at(2).props().label.toLowerCase()).toBe('75%');
-        expect(percentageButton.at(3).props().label.toLowerCase()).toBe('100%');
+        expect(
+            percentageButton
+                .at(0)
+                .props()
+                .label.toLowerCase(),
+        ).toBe('25%');
+        expect(
+            percentageButton
+                .at(1)
+                .props()
+                .label.toLowerCase(),
+        ).toBe('50%');
+        expect(
+            percentageButton
+                .at(2)
+                .props()
+                .label.toLowerCase(),
+        ).toBe('75%');
+        expect(
+            percentageButton
+                .at(3)
+                .props()
+                .label.toLowerCase(),
+        ).toBe('100%');
     });
 
     it('should render submit button', () => {
@@ -86,7 +102,9 @@ describe('OrderForm', () => {
         const submitButton = wrapper.find(Button);
         submitButton.simulate('click');
 
-        expect(onSubmit.calledOnceWith({type: 'buy', orderType: 'Limit', price: nextState.price, amount: nextState.amount})).toBeTruthy();
+        expect(
+            onSubmit.calledOnceWith({ type: 'buy', orderType: 'Limit', price: nextState.price, amount: nextState.amount }),
+        ).toBeTruthy();
     });
 
     it('should disable button if price is 0 in Limit order', () => {
@@ -147,7 +165,7 @@ describe('OrderForm', () => {
     it('should disable button amount 0 in Market order', () => {
         const onSubmit: SinonSpy = spy();
         const wrapper = setup({ onSubmit });
-        const orderState = { orderType: 'Market'};
+        const orderState = { orderType: 'Market' };
         wrapper.setState(orderState);
 
         let amountState = { amount: '0.001' };
@@ -163,7 +181,14 @@ describe('OrderForm', () => {
         expect(submitButton.props().disabled).toBe(true);
     });
 
-    const findInputAmount = (wrapper: ShallowWrapper) => wrapper.find('.cr-order-item').at(2).find('div').last().children().last();
+    const findInputAmount = (wrapper: ShallowWrapper) =>
+        wrapper
+            .find('.cr-order-item')
+            .at(2)
+            .find('div')
+            .last()
+            .children()
+            .last();
 
     it('does not change positive well formatted integers', () => {
         const wrapper = setup();
@@ -252,7 +277,7 @@ describe('OrderForm', () => {
     it('should set correct values by percentage buttons when type is buy', () => {
         const fee = 0.0001;
         const type = 'buy';
-        const wrapper = setup({fee, type});
+        const wrapper = setup({ fee, type });
         const nextState = { price: '2' };
 
         wrapper.setState(nextState);
@@ -286,7 +311,7 @@ describe('OrderForm', () => {
     it('should set correct values by percentage buttons when type is sell', () => {
         const fee = 0.0001;
         const type = 'sell';
-        const wrapper = setup({fee, type});
+        const wrapper = setup({ fee, type });
         const nextState = { price: '2' };
 
         wrapper.setState(nextState);
@@ -321,7 +346,7 @@ describe('OrderForm', () => {
         const fee = 0.0001;
         const type = 'buy';
         const proposals = [['2', '200']];
-        const wrapper = setup({fee, type, proposals});
+        const wrapper = setup({ fee, type, proposals });
         const nextState = { total: '0', price: '2' };
 
         wrapper.setState(nextState);
@@ -331,13 +356,21 @@ describe('OrderForm', () => {
 
         expect(wrapper.state('amount')).toBe('110');
 
-        const total = wrapper.find('.cr-order-item__total').find('.cr-order-item__total__content').find('.cr-order-item__total__content__amount').props().children;
-        const available = wrapper.find('.cr-order-item__available').find('.cr-order-item__available__content').find('.cr-order-item__available__content__amount').props().children;
-        const estimatedFee = wrapper.find('.cr-order-item__fee').find('.cr-order-item__fee__content').find('.cr-order-item__fee__content__amount').props().children;
+        const total = wrapper
+            .find('.cr-order-item__total')
+            .find('.cr-order-item__total__content')
+            .find('.cr-order-item__total__content__amount')
+            .props().children;
+        const available = wrapper
+            .find('.cr-order-item__available')
+            .find('.cr-order-item__available__content')
+            .find('.cr-order-item__available__content__amount')
+            .props().children;
+        // const estimatedFee = wrapper.find('.cr-order-item__fee').find('.cr-order-item__fee__content').find('.cr-order-item__fee__content__amount').props().children;
 
         expect(total).toEqual('220.0000');
         expect(available).toEqual('50.0000');
-        expect(estimatedFee).toEqual('0.0110');
+        // expect(estimatedFee).toEqual('0.0110');
     });
 
     it('should display values with correct precision', () => {
@@ -346,7 +379,7 @@ describe('OrderForm', () => {
         const currentMarketAskPrecision = 3;
         const currentMarketBidPrecision = 2;
         const proposals = [['2', '5']];
-        const wrapper = setup({fee, type, currentMarketAskPrecision, currentMarketBidPrecision, proposals});
+        const wrapper = setup({ fee, type, currentMarketAskPrecision, currentMarketBidPrecision, proposals });
         const nextState = { total: '0', price: '2' };
 
         wrapper.setState(nextState);
@@ -356,19 +389,31 @@ describe('OrderForm', () => {
 
         expect(wrapper.state('amount')).toBe('0.1');
 
-        const total = wrapper.find('.cr-order-item__total').find('.cr-order-item__total__content').find('.cr-order-item__total__content__amount').props().children;
-        const available = wrapper.find('.cr-order-item__available').find('.cr-order-item__available__content').find('.cr-order-item__available__content__amount').props().children;
-        const estimatedFee = wrapper.find('.cr-order-item__fee').find('.cr-order-item__fee__content').find('.cr-order-item__fee__content__amount').props().children;
+        const total = wrapper
+            .find('.cr-order-item__total')
+            .find('.cr-order-item__total__content')
+            .find('.cr-order-item__total__content__amount')
+            .props().children;
+        const available = wrapper
+            .find('.cr-order-item__available')
+            .find('.cr-order-item__available__content')
+            .find('.cr-order-item__available__content__amount')
+            .props().children;
+        // const estimatedFee = wrapper
+        //     .find('.cr-order-item__fee')
+        //     .find('.cr-order-item__fee__content')
+        //     .find('.cr-order-item__fee__content__amount')
+        //     .props().children;
 
         expect(total).toEqual('0.200');
         expect(available).toEqual('50.000');
-        expect(estimatedFee).toEqual('0.000');
+        // expect(estimatedFee).toEqual('0.000');
     });
 
     it('should display correct value of estimated fee when type is sell', () => {
         const fee = 0.0002;
         const type = 'sell';
-        const wrapper = setup({fee, type});
+        const wrapper = setup({ fee, type });
         const nextState = { total: '0', price: '2' };
 
         wrapper.setState(nextState);
@@ -376,13 +421,23 @@ describe('OrderForm', () => {
         const amountState = { amount: '110' };
         wrapper.setState(amountState);
 
-        const estimatedFee = wrapper.find('.cr-order-item__fee').find('.cr-order-item__fee__content').find('.cr-order-item__fee__content__amount').props().children;
+        // const estimatedFee = wrapper
+        //     .find('.cr-order-item__fee')
+        //     .find('.cr-order-item__fee__content')
+        //     .find('.cr-order-item__fee__content__amount')
+        //     .props().children;
 
-        expect(estimatedFee).toEqual('0.0440');
+        // expect(estimatedFee).toEqual('0.0440');
     });
 
-
-    const findInputPrice = (wrapper: ShallowWrapper) => wrapper.find('.cr-order-item').at(1).find('div').last().children().last();
+    const findInputPrice = (wrapper: ShallowWrapper) =>
+        wrapper
+            .find('.cr-order-item')
+            .at(1)
+            .find('div')
+            .last()
+            .children()
+            .last();
 
     it('should handle change price when it was set as priceLimit', () => {
         const wrapper = setup();
