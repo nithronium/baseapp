1e401d143eb6f46bf277f7893d4ce9212b416cd1
Mikhail Zyranau <mikhail.zyranau@digiwrld.com>
2019-12-03 11:53:08 +0300

    Remove /en from url => SEO req.

--- src/custom/helpers/buildPath.ts
+++ src/custom/helpers/buildPath.ts
@@ -1,1 +1,6 @@
-export const buildPath = (path: string, lang: string) => `/${lang.toLowerCase()}${path}`;
+export const buildPath = (path: string, lang: string) => {
+  const url = lang.toLowerCase() === 'en' ? path : `/${lang.toLowerCase()}${path}`;
+  // tslint:disable-next-line: no-console
+  // console.log(url);
+  return url;
+};
