fddb1f02ceb7d3c4278e4da91f79b046706fff8c
Mikhail Zyranau <mikhail.zyranau@digiwrld.com>
2019-12-15 18:46:26 +0300

    Add ru to ieo routes

--- src/plugins/ieo/containers/IEORoutes/index.tsx
+++ src/plugins/ieo/containers/IEORoutes/index.tsx
@@ -6,5 +6,7 @@ export const ieoRoutes = index => {
     return ([
         <Route exact={true} path="/ieo" component={IEOListScreen} key={index} />,
         <Route exact={true} path="/ieo/:id" component={IEODetailsScreen} key={index} />,
+        <Route exact={true} path="/ru/ieo" component={IEOListScreen} key={index} />,
+        <Route exact={true} path="/ru/ieo/:id" component={IEODetailsScreen} key={index} />,
     ]);
 };
