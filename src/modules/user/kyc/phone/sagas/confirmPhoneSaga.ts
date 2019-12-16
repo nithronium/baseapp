f88ac149782dd374994031d68c53539b5f95f972
Mikhail Zyranau <mikhail.zyranau@digiwrld.com>
2019-12-16 12:08:19 +0300

    Fix phone verification

--- src/modules/user/kyc/phone/sagas/confirmPhoneSaga.ts
+++ src/modules/user/kyc/phone/sagas/confirmPhoneSaga.ts
@@ -2,7 +2,7 @@
 import { call, put } from 'redux-saga/effects';
 import { API, RequestOptions } from '../../../../../api';
 import { alertPush } from '../../../../public/alert';
-import { changeUserLevel } from '../../../profile';
+
 import {
     SendCodeFetch,
     verifyPhoneData,
@@ -17,7 +17,6 @@ export function* confirmPhoneSaga(action: SendCodeFetch) {
     try {
         yield call(API.post(sessionsConfig), '/resource/phones/verify', action.payload);
         yield put(verifyPhoneData({ message: 'success.phone.confirmation.message' }));
-        yield put(changeUserLevel({ level: 2 }));
         yield put(alertPush({message: ['success.phone.confirmed'], type: 'success'}));
     } catch (error) {
         yield put(verifyPhoneError(error));
