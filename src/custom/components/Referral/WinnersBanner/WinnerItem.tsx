380aeee9e3316824316c6d467e9fa58ab8e29596
Andrey Elizarov <andrey.elizarov@digiwrld.com>
2019-12-25 12:32:32 +0300

    Feature: banner with winners of 1 round

---
+++ src/custom/components/Referral/WinnersBanner/WinnerItem.tsx
@@ -0,0 +1,29 @@
+/* tslint:disable */
+import * as React from 'react';
+import { InjectedIntlProps, injectIntl } from 'react-intl';
+
+export interface Winner {
+    number: number;
+    value: number;
+    currency: string;
+}
+
+
+interface WinnerItemProps extends InjectedIntlProps{
+    data: Winner;
+}
+
+export const WinnerItem: React.FC<WinnerItemProps> = injectIntl(({data, intl}) => (
+    <div className="winner-list-item">
+        <span className="winner-item-number">{intl.formatMessage({id: 'page.referral.winnersbanner.ticketnum'})}{data.number}</span>
+        <div>
+            <span className="winner-item-value">{`$${data.value} in`} </span>
+            <span className="winner-item-cur">{data.currency}</span>
+            {data.currency === 'EMRX' &&
+            <div>
+                <span className="winner-item-value"> + {`$${data.value} in`} </span>
+                <span className="winner-item-cur">BTC</span>
+            </div>
+            }
+        </div>
+    </div>));
