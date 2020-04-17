61092b5de9e9d86965aa7fb47746208ed1556ab8
oyershov <oyershov@heliostech.fr>
2020-04-17 17:29:22 +0300

    Fix: change code indentation

--- src/modules/public/orderBook/selectors.ts
+++ src/modules/public/orderBook/selectors.ts
@@ -3,21 +3,21 @@ import { RootState } from '../../index';
 import { DepthIncrementState, DepthState, OrderBookState } from './types';
 
 export const selectOrderBookLoading = (state: RootState): boolean =>
-  state.public.orderBook.loading;
+    state.public.orderBook.loading;
 
 export const selectOrderBook = (state: RootState): OrderBookState =>
   state.public.orderBook;
 
 export const selectDepthAsks = incrementalOrderBook() ?
-  (state: RootState): DepthState['asks'] => state.public.incrementDepth.asks :
-  (state: RootState): DepthIncrementState['asks'] => state.public.depth.asks;
+    (state: RootState): DepthState['asks'] => state.public.incrementDepth.asks :
+    (state: RootState): DepthIncrementState['asks'] => state.public.depth.asks;
 
 export const selectDepthBids = incrementalOrderBook() ?
-  (state: RootState): DepthState['bids'] => state.public.incrementDepth.bids :
-  (state: RootState): DepthIncrementState['bids'] => state.public.depth.bids;
+    (state: RootState): DepthState['bids'] => state.public.incrementDepth.bids :
+    (state: RootState): DepthIncrementState['bids'] => state.public.depth.bids;
 
 export const selectDepthLoading = incrementalOrderBook() ?
-  (state: RootState): boolean => state.public.incrementDepth.loading :
-  (state: RootState): boolean => state.public.depth.loading;
+    (state: RootState): boolean => state.public.incrementDepth.loading :
+    (state: RootState): boolean => state.public.depth.loading;
 
 export const selectOrderBookSequence = (state: RootState): number | null => state.public.incrementDepth.sequence;
