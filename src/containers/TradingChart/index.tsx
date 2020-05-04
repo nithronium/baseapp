4aa1f3d65a41350f50fb791879f6bb14ca64fd55
Sergei Sidoruk <alf.cat.loverr@gmail.com>
2020-05-04 12:21:57 +0300

    Fix: test issue with tranding chart

--- src/containers/TradingChart/index.tsx
+++ src/containers/TradingChart/index.tsx
@@ -64,19 +64,31 @@ export class TradingChartComponent extends React.PureComponent<Props> {
     private datafeed = dataFeedObject(this, this.props.markets);
 
     public componentWillReceiveProps(next: Props) {
+        // tslint:disable-next-line:no-console
+        console.log('...........componentWillReceiveProps');
         if (next.currentMarket && next.colorTheme && next.colorTheme !== this.props.colorTheme) {
+            // tslint:disable-next-line:no-console
+            console.log('...........first if');
             this.setChart(next.markets, next.currentMarket, next.colorTheme);
         }
 
         if (next.currentMarket && (!this.props.currentMarket || next.currentMarket.id !== this.props.currentMarket.id)) {
+            // tslint:disable-next-line:no-console
+            console.log('...........second if');
             if (this.props.currentMarket && (this.props.currentMarket.id && this.tvWidget)) {
+                // tslint:disable-next-line:no-console
+                console.log('...........3 id');
                 this.updateChart(next.currentMarket);
             } else {
+                // tslint:disable-next-line:no-console
+                console.log('...........else');
                 this.setChart(next.markets, next.currentMarket, next.colorTheme);
             }
         }
 
         if (next.kline && next.kline !== this.props.kline) {
+            // tslint:disable-next-line:no-console
+            console.log('...........4 if');
             this.datafeed.onRealtimeCallback(next.kline);
         }
     }
@@ -87,8 +99,11 @@ export class TradingChartComponent extends React.PureComponent<Props> {
             currentMarket,
             markets,
         } = this.props;
-
+        // tslint:disable-next-line:no-console
+        console.log('...........test', this.props);
         if (currentMarket) {
+            // tslint:disable-next-line:no-console
+            console.log('...........markets, currentMarket, colorTheme', markets, currentMarket, colorTheme);
             this.setChart(markets, currentMarket, colorTheme);
         }
     }
