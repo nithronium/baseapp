77da181225b70c80be1c85568c556ea87e7e1fef
Sergei Sidoruk <alf.cat.loverr@gmail.com>
2020-05-06 15:43:23 +0300

    Test color theme

--- src/containers/TradingChart/index.tsx
+++ src/containers/TradingChart/index.tsx
@@ -71,8 +71,12 @@ export class TradingChartComponent extends React.PureComponent<Props> {
         if (next.currentMarket && (!this.props.currentMarket || next.currentMarket.id !== this.props.currentMarket.id)) {
             if (this.props.currentMarket && (this.props.currentMarket.id && this.tvWidget)) {
                 this.tvWidget.remove();
+                // tslint:disable-next-line:no-console
+                console.log('...........next.colorTheme', next.colorTheme);
                 this.setChart(next.markets, next.currentMarket, next.colorTheme);
             } else {
+                // tslint:disable-next-line:no-console
+                console.log('...........next.colorTheme', next.colorTheme);
                 this.setChart(next.markets, next.currentMarket, next.colorTheme);
             }
         }
