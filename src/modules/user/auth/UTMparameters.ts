982f3944792f81d9d5ce2aad63514fb1e3779988
Sergei Sidoruk <alf.cat.loverr@gmail.com>
2020-06-18 12:25:05 +0300

    Update save parameters

--- src/modules/user/auth/UTMparameters.ts
+++ src/modules/user/auth/UTMparameters.ts
@@ -1,31 +1,31 @@
 export const getParameters = () => {
-    const utmSource = localStorage.getItem('utmSource');
-    const utmMedium = localStorage.getItem('utmMedium');
-    const utmCampaign = localStorage.getItem('utmCampaign');
-    const utmContent = localStorage.getItem('utmContent');
-    const utmTerm = localStorage.getItem('utmTerm');
-    const refid = localStorage.getItem('referralCode');
+    const utmSource = localStorage.getItem('utm_source');
+    const utmMedium = localStorage.getItem('utm_medium');
+    const utmCampaign = localStorage.getItem('utm_campaign');
+    const utmContent = localStorage.getItem('utm_content');
+    const utmTerm = localStorage.getItem('utm_term');
+    const refid = localStorage.getItem('refid');
     const data = {};
     // tslint:disable-next-line
-    if (utmSource) { data['utmSource'] = utmSource }
+    if (utmSource) { data['utm_source'] = utmSource }
     // tslint:disable-next-line
-    if (utmMedium) { data['utmMedium'] = utmMedium }
+    if (utmMedium) { data['utm_medium'] = utmMedium }
     // tslint:disable-next-line
-    if (utmCampaign) { data['utmCampaign'] = utmCampaign }
+    if (utmCampaign) { data['utm_campaign'] = utmCampaign }
     // tslint:disable-next-line
-    if (utmContent) { data['utmContent'] = utmContent }
+    if (utmContent) { data['utm_content'] = utmContent }
     // tslint:disable-next-line
-    if (utmTerm) { data['utmTerm'] = utmTerm }
+    if (utmTerm) { data['utm_term'] = utmTerm }
     // tslint:disable-next-line
     if (refid) { data['refid'] = refid }
     return Object.keys(data).length > 0 ? data : false;
 };
 
 export const removeParameters = () => {
-    localStorage.removeItem('utmSource');
-    localStorage.removeItem('utmMedium');
-    localStorage.removeItem('utmCampaign');
-    localStorage.removeItem('utmContent');
-    localStorage.removeItem('utmTerm');
-    localStorage.removeItem('referralCode');
+    localStorage.removeItem('utm_source');
+    localStorage.removeItem('utm_medium');
+    localStorage.removeItem('utm_campaign');
+    localStorage.removeItem('utm_content');
+    localStorage.removeItem('utm_term');
+    localStorage.removeItem('refid');
 };
