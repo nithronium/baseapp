278bff8ec7ae7134c33f43f75cc041a38dc0fd12
Sergei Sidoruk <alf.cat.loverr@gmail.com>
2020-07-02 17:44:12 +0300

    Update block resend qustionnaire

--- src/helpers/checkQuestionnaire.ts
+++ src/helpers/checkQuestionnaire.ts
@@ -1,6 +1,6 @@
 export const checkQuestionnaire = labels => {
     return labels.reduce((isQuestionnaire, obj) => {
-        if (isQuestionnaire || obj.key === 'questionnaireForm') {
+        if (isQuestionnaire || obj.key === 'questionnaireform') {
             return true;
         }
         return isQuestionnaire;
