2342d62006a1790ebb79d36d437bdbf0249f2589
Sergey Sidoruk <sergey.sidoruk@emirex.com>
2020-07-24 14:54:36 +0300

    Fix: Update save parameters before redirect to confirm

--- src/custom/helpers/redirectUrl.ts
+++ src/custom/helpers/redirectUrl.ts
@@ -2,9 +2,17 @@
 import * as qs from 'qs';
 
 export const getRedirectUrl = () => {
-    const parsed = qs.parse(window.location.search, { ignoreQueryPrefix: true });
+    const parsed = qs.parse(location.search, { ignoreQueryPrefix: true });
+    if (parsed.redirect_url) {
+        let query = parsed.redirect_url;
+        if (parsed.fiat && parsed.crypto && parsed.fiatValue) {
+            query += `&fiat=${parsed.fiat}&crypto=${parsed.crypto}&fiatValue=${parsed.fiatValue}`;
+        }
 
-    return parsed.redirect_url;
+        return query;
+    }
+
+    return false;
 };
 
 export const buildUrlWithRedirect = url => {
